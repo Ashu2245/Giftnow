f72a284790109591a28ebc13e62cf1136757670e
vishal <excellencetechnologies08@gmail.com>
2017-08-05 18:05:32 +0530

    create header component

---
+++ src/component/header/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { HEXCOLOR } from '../../style/hexcolor';
+
+const style = StyleSheet.create({
+  headerColor: {
+    backgroundColor: 'transparent',
+  },
+  headerTitle: {
+    color: HEXCOLOR.WhiteColor,
+  },
+});
+
+export default style;
