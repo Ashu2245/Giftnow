b92c313c5910a43eee319dd213d89de5ae0639a3
codeashu <aashu2245@gmail.com>
2017-08-06 04:09:53 +0530

    Drawer Content with icon basic design completed

--- src/component/drawercontent/drawercontent.js
+++ src/component/drawercontent/drawercontent.js
@@ -5,6 +5,7 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity
 } from 'react-native';
 import { Button, Icon, Thumbnail } from 'native-base';
 import { NavigationActions } from 'react-navigation';
@@ -43,12 +44,40 @@ class DrawerContent extends Component {
   render() {
     return (
       <ScrollView style={styles.container}>
+        <View style={{flex:1  }}>
         <View style={{marginTop:10, flexDirection:'row'}}>
            <Thumbnail  style={{marginTop:10}} source={{uri: 'https://cdn2.iconfinder.com/data/icons/avatar-2/512/john_man_face-512.png'}} />
           <View style={{marginLeft:10,marginTop:9,borderBottomWidth:0.25,borderColor:'grey'}} >
             <Text style={{fontSize:25, color:'black'}}>Ashutosh Pandey</Text>
             <Text style={{fontSize:14.5, color:'grey',marginBottom:10}}>codeashu@gmail.com</Text>
           </View>
+        </View>
+        <View style={{marginTop:40,marginLeft:3}}>
+          <TouchableOpacity style={{flexDirection:'row',}}>
+            <Icon name='ios-home-outline'/>
+            <Text style={{fontSize:16,marginTop:2, marginLeft:27}}>
+              Home
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginTop:20,flexDirection:'row'}}>
+            <Icon name='ios-person-add-outline'/>
+            <Text style={{fontSize:16,marginTop:2, marginLeft:29}}>
+              Profile
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginTop:20,flexDirection:'row'}}>
+            <Icon name='ios-settings-outline'/>
+            <Text style={{fontSize:16,marginTop:2, marginLeft:26}}>
+              Setting
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginTop:20, flexDirection:'row'}}>
+            <Icon name='ios-log-out-outline'/>
+            <Text style={{fontSize:16,marginTop:2, marginLeft:25}}>
+              Sign Out
+            </Text>
+          </TouchableOpacity>
+        </View>
         </View>
       </ScrollView>
     );
