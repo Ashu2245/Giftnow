d1e678cae8e818fa91339e6202b55a282233cb67
vishal <excellencetechnologies08@gmail.com>
2017-08-14 11:27:24 +0530

    bug

---
+++ src/component/action/constant.js
@@ -0,0 +1,9 @@
+// Login in HrReruit App
+export const USER_LOGIN_REQUEST = 'auth/login/request';
+export const USER_LOGIN_SUCCESS = 'auth/login/success';
+export const USER_LOGIN_FAILED = 'auth/login/failed';
+
+// Signout From App
+export const USER_LOGOUT_REQUEST = 'auth/logout/request';
+export const USER_LOGOUT_SUCCESS = 'auth/logout/success';
+export const USER_LOGOUT_FAILED = 'auth/logout/failed';
